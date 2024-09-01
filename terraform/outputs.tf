45f18d89a7c2006da3156a0c8338b7a4e17b72e9
Duc Le <ducle@DiMac>
2024-09-01 13:46:34 +0700

    fix public_ip

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,6 @@
 output "public_ip" {
-  value = aws_instance.django_server.public_ip
+  value = length(aws_instance.django_server) > 0 ? aws_instance.django_server[0].public_ip : null
+  description = "The public IP of the Django server (if created)"
 }
 
 output "key_name" {
