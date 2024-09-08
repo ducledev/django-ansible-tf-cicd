c74827fdd308b8c3ea9f2354d676086e0b80fa50
Duc Le <ducle@DiMac>
2024-09-08 15:28:18 +0700

    set default of disk size to 16GB

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,11 @@ resource "aws_instance" "django_server" {
   vpc_security_group_ids = [aws_security_group.django_sg.id]
   subnet_id                   = aws_subnet.django_demo_public_subnet.id
   associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 16  # Explicitly set to 16 GB
+    volume_type = "gp3"
+  }
 }
 
 resource "aws_security_group" "django_sg" {
